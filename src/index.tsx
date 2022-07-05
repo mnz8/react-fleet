cd4f0026be3c2ecf082acd5e965ef71ac45644df
mnz8 <mnz8gg@gmail.com>
2022-07-05 20:12:02 +0800

    feat: 启动

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>Bench</div>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
