e3a11163abb72b67549b848ea71c5cfd4cc04ecb
line126 <line112266@126.com>
2022-08-18 21:29:11 +0800

    feat: add lint-staged

--- src/pages/PageOne.tsx
+++ src/pages/PageOne.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { flushSync } from "react-dom";
+import React, { useState, useEffect } from 'react';
+import { flushSync } from 'react-dom';
 
 function PageOne() {
   return <div>PageOne</div>;
