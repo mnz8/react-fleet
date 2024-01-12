3064124e9ff631225fdb1ccf65740ffd1cafa973
line126 <line112266@126.com>
2024-01-13 00:28:38 +0800

    updated periodically

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, useRoutes } from 'react-router-dom';
-import routes from '../config/routes';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import routes, { type typeMyRoute } from '../config/routes';
+
+function generateRoutes(routerItems: typeMyRoute[]) {
+  if (routerItems && routerItems.length) {
+    return routerItems.map(({ path, element, children }) =>
+      children && children.length ? (
+        <Route path={path} key={path} element={element}>
+          {generateRoutes(children)}
+        </Route>
+      ) : (
+        <Route key={path} path={path} element={element} />
+      ),
+    );
+  }
+  return null;
+}
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container!);
 
 const App: React.FC = () => {
-  let element = useRoutes(routes);
-  return element;
+  const element = generateRoutes(routes);
+  return <Routes>{element}</Routes>;
 };
 
 root.render(
